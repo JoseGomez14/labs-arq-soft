d80409797873a37000318c41ab3ed4652c24a375
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-03-15 09:37:43 -0500

    feat: :package: Add flight entity model
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

---
+++ lab-1/backend/src/main/java/co/edu/udea/backend/model/Flight.java
@@ -0,0 +1,126 @@
+package co.edu.udea.backend.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+
+import java.time.LocalDate;
+
+/**
+ * This class represents a flight entity.
+ */
+public class Flight {
+    /**
+     * Unique identifier of the flight entity, assigned as a sequence of integers.
+     */
+    private int id;
+    /**
+     * Name of the airline operating the flight
+     */
+    private String airline;
+    /**
+     * Name of city from which the flight departs
+     */
+    private String origin;
+    /**
+     * Name of city to which the flight arrives
+     */
+    private String destination;
+    /**
+     * The flight price in dollars.
+     */
+    private int price;
+    /**
+     * The departure date and time of the flight.
+     */
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate departureDate;
+    /**
+     * The arrival date and time of the flight.
+     */
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate arrivalDate;
+
+    /**
+     * Default constructor
+     */
+    public Flight() {
+    }
+
+    /**
+     * Constructor with all parameters
+     *
+     * @param id            Identifier of the flight entity
+     * @param airline       Name of the airline operating the flight
+     * @param origin        Name of city from which the flight departs
+     * @param destination   Name of city to which the flight arrives
+     * @param price         Flight price in dollars
+     * @param departureDate The departure date and time of the flight
+     * @param arrivalDate   The arrival date and time of the flight
+     */
+    public Flight(int id, String airline, String origin, String destination, int price, LocalDate departureDate, LocalDate arrivalDate) {
+        this.id = id;
+        this.airline = airline;
+        this.origin = origin;
+        this.destination = destination;
+        this.price = price;
+        this.departureDate = departureDate;
+        this.arrivalDate = arrivalDate;
+    }
+
+    // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public LocalDate getDepartureDate() {
+        return departureDate;
+    }
+
+    public void setDepartureDate(LocalDate departureDate) {
+        this.departureDate = departureDate;
+    }
+
+    public LocalDate getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public void setArrivalDate(LocalDate arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+}
