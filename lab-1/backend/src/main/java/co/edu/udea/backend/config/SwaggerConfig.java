cf6d42e1386fed546912e1c2248e8314050795c6
JoseGomez14 <jose.gomez14@udea.edu.co>
2024-03-15 08:55:47 -0500

    docs: :heavy_plus_sign: Update dependencies and config for Api docs
    Co-authored-by: daniel-lujan <daniel.lujan@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

--- lab-1/backend/src/main/java/co/edu/udea/backend/config/SwaggerConfig.java
+++ lab-1/backend/src/main/java/co/edu/udea/backend/config/SwaggerConfig.java
@@ -1,14 +1,32 @@
 package co.edu.udea.backend.config;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
-@OpenAPIDefinition
-
 public class SwaggerConfig {
-    public OpenAPI api(){
-        return new OpenAPI();
+
+    @Value("http://localhost:8080")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Info info = new Info()
+                .title("Lab 1 - REST API Documentation")
+                .version("1.0")
+                .description("REST API Documentation for Lab 1 of the Software Architecture course");
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
     }
 }
