335fdc1402e2077052153eb63003a5059d88894e
daniel-lujan <daniel.lujan@udea.edu.co>
2024-03-17 23:31:02 -0500

    feat: :sparkles: Add FlightsList component
    Co-authored-by: JoseGomez14 <jose.gomez14@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

--- lab-1/frontend/src/components/Flights/FilghtsList.jsx
+++ lab-1/frontend/src/components/Flights/FilghtsList.jsx
@@ -1,5 +1,24 @@
-const FilghtsList = () => {
-  return <div>FilghtsList</div>;
+import FlightCard from "./FlightCard";
+
+const FilghtsList = ({ flights }) => {
+  return (
+    <div>
+      <div className="space-y-2">
+        <h2 className="text-2xl font-semibold">Flights</h2>
+        <div className="h-2" />
+      </div>
+      <div className="space-y-4 flex gap-10 flex-col">
+        {flights.map((flight) => {
+          return (
+            <div key={flight.id + "flight"}>
+              <FlightCard flight={flight} />
+              <div className="border-t border-gray-200 w-full" />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default FilghtsList;
