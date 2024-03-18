d3bd104fb448365e234bf1d3cf73027eb167d90d
daniel-lujan <daniel.lujan@udea.edu.co>
2024-03-17 23:32:32 -0500

    feat: :sparkles: Add flight results and search to FlightSearch page
    Co-authored-by: JoseGomez14 <jose.gomez14@udea.edu.co>
    Co-authored-by: GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
    Co-authored-by: anavegaa <ana.vega@udea.edu.co>
    Co-authored-by: MaritzaTC <maritza.tabarezc@udea.edu.co>
    Co-authored-by: JuanPablo-A <jpablo.arango1@udea.edu.co>

--- lab-1/frontend/src/app/flightsSearch/page.js
+++ lab-1/frontend/src/app/flightsSearch/page.js
@@ -1,9 +1,20 @@
-import Styles from '@/styles/page.module.css'
+"use client";
+
+import FilghtsList from "@/components/Flights/FilghtsList";
+import { FilterBox } from "@/components/Search/FilterBox";
+import { useState } from "react";
 
 export default function FlightsSearch() {
+  const [flights, setFlights] = useState([]);
+
   return (
-    <main className={Styles.main}>
-      <h1>Flights Search Page</h1>
+    <main className="w-full max-w-3xl mx-auto px-4 mt-8">
+      <h2 className="text-2xl font-semibold">¡Find your perfect flight!</h2>
+      <br />
+      <div className="space-y-6">
+        <FilterBox setFlights={setFlights} />
+        <FilghtsList flights={flights} />
+      </div>
     </main>
   );
 }
